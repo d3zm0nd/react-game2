9c3063b7fd4592fcd7045e069fbea85fba32ad8e
d3zm0nd <79636873+d3zm0nd@users.noreply.github.com>
2021-03-05 00:20:51 +0300

    lesson 4

--- src/components/Storage/Storage.js
+++ src/components/Storage/Storage.js
@@ -1,18 +1,39 @@
-import './Storage.css';
+import './Storage.scss';
 
 function Storage(props) {
+
   const findGoodById = (id) => {
     return props.goods.find((item) => {
       return item.id === id;
     }).title;
   }
+
+  const getEmptyCells = (count) => {
+    if (count < 8) {
+      return Array(8-count).fill().map(() => {
+        return (
+          <li className='good-item'></li>
+        )
+      })
+    }
+  }
+
   return (
     <div>
-      {props.storage.map((item) => {
-        return (
-          <span>{item.id}. {findGoodById(item.id)} {item.qty} шт.<br/></span>
-        )
-      })}
+      <h2 className="title">Мой склад</h2>
+
+      <div className="panel">
+        <ul className="goods">
+          {props.storage.map((item) => {
+            return (
+              <li className="good-item">
+                {findGoodById(item.id)}, {item.qty} шт.</li>
+            )
+          })}
+
+          {getEmptyCells(props.storage.length)}
+        </ul>
+      </div>
     </div>
   );
 }

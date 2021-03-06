e768ad7cf7180bba16ebd1221f00b85ca98b6657
d3zm0nd <79636873+d3zm0nd@users.noreply.github.com>
2021-03-06 03:23:34 +0300

    lesson 6: add data file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,115 +6,14 @@ import CityStorage from '../CityStorage/CityStorage';
 import Storage from '../Storage/Storage';
 import Transportation from '../Transportation/Transportation';
 import Stats from '../Stats/Stats';
+import { storages as storagesData, goods } from '../../data';
 
 function App() {
   const [currentCity, setCurrentCity] = useState(1);
-  const [storages, setStorages] = useState([
-    {
-      cityId: 1,
-      storage: [
-        {
-          id: 1,
-          qty: 10,
-        },
-        {
-          id: 2,
-          qty: 20,
-        },
-        {
-          id: 3,
-          qty: 204,
-        },
-        {
-          id: 4,
-          qty: 200,
-        },
-        {
-          id: 5,
-          qty: 120,
-        },
-        {
-          id: 6,
-          qty: 10,
-        },
-        {
-          id: 7,
-          qty: 2,
-        },
-      ],
-    },
-    {
-      cityId: 2,
-      storage: [
-        {
-          id: 1,
-          qty: 5,
-        },
-      ],
-    },
-  ]);
+  const [storages, setStorages] = useState(storagesData);
   const [money, setMoney] = useState(1000);
   const [days, setDays] = useState(1);
   const [selectedGood, setSelectedGood] = useState(1)
-
-  const goods = [
-    {
-      id: 1,
-      title: "Пиво",
-    },
-    {
-      id: 2,
-      title: "Молоко",
-    },
-    {
-      id: 3,
-      title: "Пшеница",
-    },
-    {
-      id: 4,
-      title: "Грибы",
-    },
-    {
-      id: 5,
-      title: "Клевер",
-    },
-    {
-      id: 6,
-      title: "Лук",
-    },
-    {
-      id: 7,
-      title: "Виноград",
-    },
-    {
-      id: 8,
-      title: "Орехи",
-    },
-    {
-      id: 9,
-      title: "Вилы",
-    },
-    {
-      id: 10,
-      title: "Доски",
-    },
-    {
-      id: 11,
-      title: "Коса",
-    },
-    {
-      id: 12,
-      title: "Лопата",
-    },
-    {
-      id: 13,
-      title: "Топор",
-    },
-    {
-      id: 14,
-      title: "Кирка",
-    },
-  ];
 
   const sellGoods = (goodId, qty) => {
     const cityStorage = storages.find((storage) => {
@@ -129,10 +28,9 @@ function App() {
       if (currentGood) {
         currentGood.qty -= qty;
         setMoney(money + qty * 10);
+        setStorages(storages);
       }
     }
-
-    setStorages(storages);
   }
 
   const getStorageByCity = () => {
---
+++ src/data.js
@@ -0,0 +1,101 @@
+export const storages = [{
+    cityId: 1,
+    storage: [
+        {
+            id: 1,
+            qty: 10,
+        },
+        {
+            id: 2,
+            qty: 20,
+        },
+        {
+            id: 3,
+            qty: 204,
+        },
+        {
+            id: 4,
+            qty: 200,
+        },
+        {
+            id: 5,
+            qty: 120,
+        },
+        {
+            id: 6,
+            qty: 10,
+        },
+        {
+            id: 7,
+            qty: 2,
+        },
+    ],
+},
+{
+    cityId: 2,
+    storage: [
+        {
+            id: 1,
+            qty: 5,
+        },
+    ],
+}];
+
+export const goods = [
+    {
+      id: 1,
+      title: "Пиво",
+    },
+    {
+      id: 2,
+      title: "Молоко",
+    },
+    {
+      id: 3,
+      title: "Пшеница",
+    },
+    {
+      id: 4,
+      title: "Грибы",
+    },
+    {
+      id: 5,
+      title: "Клевер",
+    },
+    {
+      id: 6,
+      title: "Лук",
+    },
+    {
+      id: 7,
+      title: "Виноград",
+    },
+    {
+      id: 8,
+      title: "Орехи",
+    },
+    {
+      id: 9,
+      title: "Вилы",
+    },
+    {
+      id: 10,
+      title: "Доски",
+    },
+    {
+      id: 11,
+      title: "Коса",
+    },
+    {
+      id: 12,
+      title: "Лопата",
+    },
+    {
+      id: 13,
+      title: "Топор",
+    },
+    {
+      id: 14,
+      title: "Кирка",
+    },
+  ];
